02951889991fd85ddb1fd5054e6a12ded1f00dd4
Ismael Valentin <ismaelvale@gmail.com>
2022-09-11 13:58:16 -0400

    updated port

--- app.js
+++ app.js
@@ -220,6 +220,6 @@ Message.updateOne({_id : req.params.id}, {comments: msg.comments }).exec(err => 
 res.redirect('/');
 });
 
-app.listen(3000, () => console.log("app listening on port 3000!"));
+app.listen(process.env.PORT || 3000, () => console.log('app listening on port 3000!'));
 
 module.exports = app;
